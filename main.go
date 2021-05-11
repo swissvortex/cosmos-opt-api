8a1908985479ebc2cba2b365ee3cda8b6d611c9c
xyz <11524882+xyzmap@users.noreply.github.com>
2021-05-11 15:24:33 +0200

    Rename repo

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"log"
+	"time"
+
 	"github.com/Colm3na/cosmos-opt-api/controller"
 	"github.com/Colm3na/cosmos-opt-api/logger"
 	"github.com/Colm3na/cosmos-opt-api/repository"
@@ -9,14 +12,32 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 
 	. "github.com/Colm3na/cosmos-opt-api/constants"
+
+	tb "gopkg.in/tucnak/telebot.v2"
 )
 
 func main() {
 	logger := logger.NewLogger()
 	logger.SetLoggingLevel(LOGGING_LEVEL)
-	cryptoRepository := repository.NewRepository(logger)
-	service := service.NewService(cryptoRepository, logger)
+	repository := repository.NewRepository(logger)
+	service := service.NewService(repository, logger)
 	api := controller.NewController(service, logger)
+
+	b, err := tb.NewBot(tb.Settings{
+		Token:  "1707302307:AAHAGVGMFs28GW1_2m8ND938X_Yu3jMYUOE",
+		Poller: &tb.LongPoller{Timeout: 10 * time.Second},
+	})
+
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	b.Handle("/hello", func(m *tb.Message) {
+		b.Send(m.Sender, "Hola jefe")
+	})
+
+	b.Start()
 
 	e := echo.New()
 	e.Use(middleware.Logger())
